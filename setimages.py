e577144f1cf227ffe9b50a5e7d30d8a1208c3ade
sienkiew <sienkiew@stsci.edu>
2010-04-30 18:16:19 +0000

    separate inserting images into common.py from install process
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@151 d34015c8-bcbb-4646-8ac8-8ba5febf221d

---
+++ setimages.py
@@ -0,0 +1,29 @@
+#
+#
+def setimages() :
+    import os
+    import base64
+    f = open( os.path.dirname(__file__) + "pandokia/head.png", "rb" )
+    header = f.read()
+    f.close()
+    header = base64.b64encode(header)
+
+    # f = open( os.path.dirname(__file__) + "pandokia/favicon.ico", "rb" )
+    # favico = f.read()
+    # f.close()
+    # favico = base64.b64encode(favico)
+    favico = 'None'
+
+    f = open( os.path.dirname(__file__) + "pandokia/common.py", "r" )
+    l = f.readlines()
+    f.close()
+
+    f = open( os.path.dirname(__file__) + "pandokia/common.py", "w" )
+    for x in l :
+        if not x.startswith("B64IMG_") :
+            f.write(x)
+    f.write("B64IMG_FAVICO = '%s'\n"%favico)
+    f.write("B64IMG_HEADER = '%s'\n"%header)
+    f.close()
+
+setimages()
