5ca2bfc066252c0cd7e18f34c4802186e0361436
sienkiew <sienkiew@stsci.edu>
2010-08-25 18:52:53 +0000

    minipyt disable test feature
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@223 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/helpers/minipyt.py
+++ pandokia/helpers/minipyt.py
@@ -1,17 +1,63 @@
-
+#
 # minipyt decorators 
 #
-# idea: make these look like nose decorators and use the same signalling,
-# so nose/minipyt tests can be more compatible
+
+__all__ = [ 'test', 'istest', 'nottest', 'disable' ]
+
+##########
+#
+# Note that you might expect the user to 
+#
+#   from pandokia.helpers.minipyt import *
+#
+# so we tag every function in this file as not a test by setting __test__
+# to false.
+
+#
+##########
+#
+# Nose compatible
+#
+# These look like nose decorators and use the same signalling,
+# so nose/minipyt tests can be more compatible.  You can use
+# these decorators with nose, and you can use nose decorators
+# for these features with minipyt.
+#
+
+###
+#
+# This thing _is_ a test
+#
 
 def test(f) :
     f.__test__ = True
     return f
 
-# in case somebody calls it by the name nose uses
+    # in case somebody calls it by the name nose uses
 istest = test
+
+test.__test__ = False
+
+###
+#
+# This this is not a test
+#
 
 def nottest(f) :
     f.__test__ = False
     return f
+
+nottest.__test__ = False
+
+##########
+#
+# features that nose does not have
+#
+
+def disable(f) :
+    f.__disable__ = True
+    return f
+
+disable.__test__ = False
+
 
