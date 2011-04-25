f9826a149e97b31d1663b208fbcbc75df9eb83c3
sienkiew <sienkiew@stsci.edu>
2011-04-25 16:39:56 +0000

    fix some problems with the mmapped status
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@315 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/entry.py
+++ pandokia/entry.py
@@ -193,6 +193,11 @@ def run() :
         # common.print_stat_dict(lstat)
         return err
 
+    if cmd == 'runstatus' :
+        import pandokia.run_status as x
+        err = x.display_interactive(args)
+        return err
+
     if cmd == 'version' or cmd == '--version' or cmd == '-v' or cmd == '-V' :
         import pandokia
         print "pandokia",pandokia.__version__
