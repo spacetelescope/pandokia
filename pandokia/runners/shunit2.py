810eb147106dc1b1b34151d567faa3cd4cb41f05
sienkiew <sienkiew@stsci.edu>
2011-09-16 16:08:36 +0000

    shunit2 fixes for windows, proposed shunit2 plugin patch
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@413 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/runners/shunit2.py
+++ pandokia/runners/shunit2.py
@@ -1,8 +1,10 @@
-
 import platform
 windows = platform.system() == 'Windows'
 
 if windows :
+    # You can't run shunit2 on Windows, so report those tests as Disable.
+    # (maybe do something for cygwin/mingw some day.)
+
     def run_internally(env) :
         f = open(env['PDK_LOG'],"a")
 
@@ -14,7 +16,7 @@ if windows :
             name = name [:-7]
         f.write("name=%s\n"%name)
 
-        f.write("status=E\nlog=shunit2 not available on Windows\nEND\n\n")
+        f.write("status=D\nlog=shunit2 not available on Windows\nEND\n\n")
         f.close()
 
     def command(env) :
@@ -22,7 +24,7 @@ if windows :
 
 else :
     def command( env ) :
-        return 'sh $PDK_FILE'
+        return 'shunit2 --plugin pdk $PDK_FILE'
 
 # returns a list of tests in the file
 def list( env ) :
