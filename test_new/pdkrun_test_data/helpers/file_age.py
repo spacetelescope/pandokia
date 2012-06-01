5ffd23cb5676ea0004f59bb984f0f332d6bcfbc1
sienkiew <sienkiew@stsci.edu>
2012-06-01 15:46:56 +0000

    clarify filenames
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@679 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- test_new/pdkrun_test_data/helpers/file_age.py
+++ test_new/pdkrun_test_data/helpers/file_age.py
@@ -14,42 +14,43 @@ def mkfile(name, age) :
     open(name,"w").close()
     os.utime(name, ( now - age, now - age ) )
 
-mkfile('t1', 3600)
-mkfile('t2', 1800)
-mkfile('t3', 86400 * 2)
+mkfile('60m', 3600)
+mkfile('30m', 1800)
+mkfile('1d', 86400 * 2)
 
-print "t1 age:",fc.file_age('t1')
-print "t2 age:",fc.file_age('t2')
+# take int to make the text compare of the log file work
+print "60m age:",int(fc.file_age('60m'))
+print "30m age:",int(fc.file_age('30m'))
 
 @mph.test
 def f_older_p() :
-    fc.assert_file_older( 't1', 't2' )
+    fc.assert_file_older( '60m', '30m' )
 
 @mph.test
 def f_newer_p() :
-    fc.assert_file_newer( 't2', 't1' )
+    fc.assert_file_newer( '30m', '60m' )
 
 @mph.test
 def f_newer_f() :
-    fc.assert_file_newer( 't1', 't2' )
+    fc.assert_file_newer( '60m', '30m' )
 
 @mph.test
 def f_older_f() :
-    fc.assert_file_older( 't2', 't1' )
+    fc.assert_file_older( '30m', '60m' )
 
 
 @mph.test
 def hour_older_p() :
-    fc.assert_file_older( 't1', hours=0.75 )
+    fc.assert_file_older( '60m', hours=0.75 )
 
 @mph.test
 def hour_older_f() :
-    fc.assert_file_older( 't1', hours=1.25 )
+    fc.assert_file_older( '60m', hours=1.25 )
 
 @mph.test
 def day_older_p() :
-    fc.assert_file_older( 't3', days=1 )
+    fc.assert_file_older( '1d', days=1 )
 
 @mph.test
 def day_older_f() :
-    fc.assert_file_older( 't3', days=3 )
+    fc.assert_file_older( '1d', days=3 )
