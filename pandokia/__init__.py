903615b953587e0802379de6fe15e1f03ff3308b
sienkiew <sienkiew@stsci.edu>
2013-07-17 20:35:10 +0000

    declare rc8
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@1003 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/__init__.py
+++ pandokia/__init__.py
@@ -13,7 +13,7 @@ describe pandokia a little
 import os
 
 # setup.py searches for this exact form of the next line:
-__version__ = "1.2rc8dev"
+__version__ = "1.2rc8"
 
 # this looks a little strange, but we are contemplating support for
 # multiple configurations; that would go here.
