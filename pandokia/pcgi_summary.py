c0600af5b98beb6662de42110cd0a27956aab462
cslocum <cslocum@stsci.edu>
2013-11-08 20:35:52 +0000

    ... (form validation)
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@1102 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/pcgi_summary.py
+++ pandokia/pcgi_summary.py
@@ -285,28 +285,16 @@ def run ( ) :
         output.write('''
     <script language=javascript type='text/javascript'>
     
-        function validate_flag_ok()
+        function validate_flag_ok(fff)
         {
             var comment = document.forms["testform"]["ok_comment"].value;
 
             if (comment == "")
             {
-                alert("Doh!  You forgot to comment!" + document.forms["testform"]["action_flagok"].value;);
+                alert("Doh!  You forgot to comment!");
                 return false;
             }
             return true;
-
-            /*
-            if (fff.name == "action_flagok")
-            {
-                if (comment == "")
-                {
-                    alert("Doh!  You forgot to comment!");
-                    return false;
-                }
-            }
-            return true;
-            */
         }
     
         function set_all(value)
