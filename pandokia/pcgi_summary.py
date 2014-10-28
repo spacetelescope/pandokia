94d934fdaeb70e7b1a04caa9be97e8f06cc9d79f
cslocum <cslocum@stsci.edu>
2014-10-28 16:08:33 +0000

    take 4
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@1343 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/pcgi_summary.py
+++ pandokia/pcgi_summary.py
@@ -458,7 +458,7 @@ def get_table( qid, sort_link, cmp_run, cmptype , show_attr):
     result_table.define_column("context",   link=sort_link+"Ucontext")
     result_table.define_column("test_name", link=sort_link+"Utest_name")
     result_table.define_column("contact",   link=sort_link+"Ucontact")
-    #result_table.define_column("start",     link=sort_link+"Ustart")
+    result_table.define_column("start",     link=sort_link+"Ustart")
     result_table.define_column("duration",  link=sort_link+"Uduration")
     if cmp_run != "" :
         result_table.define_column("diff",  link=sort_link+"Udiff")
@@ -538,7 +538,7 @@ def get_table( qid, sort_link, cmp_run, cmptype , show_attr):
 
         start_time = lib.decode_time_float(start_time)
         end_time   = lib.decode_time_float(end_time)
-        result_table.set_value(rowcount,"start", lib.decode_time_str(start_time))
+        #result_table.set_value(rowcount,"start", lib.decode_time_str(start_time))
         if start_time is not None and end_time is not None :
             result_table.set_value(rowcount,"duration", "%.3f" % (end_time - start_time) )
             
