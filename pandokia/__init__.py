6efeee3e09f155a6179f528724ad70d0d080568c
sienkiew <sienkiew@stsci.edu>
2014-11-18 18:01:53 +0000

    next dev release number
    git-svn-id: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk@1352 d34015c8-bcbb-4646-8ac8-8ba5febf221d

--- pandokia/__init__.py
+++ pandokia/__init__.py
@@ -13,7 +13,7 @@ describe pandokia a little
 import os
 
 # setup.py searches for this exact form of the next line:
-__version__ = "1.3.7"
+__version__ = "1.3.8dev"
 
 # this looks a little strange, but we are contemplating support for
 # multiple configurations; that would go here.
