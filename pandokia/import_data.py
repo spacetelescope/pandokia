13a696e3c4e44d80284cc09603d446ff01f2f4c0
Oi In Tam <oiin.tam@gmail.com>
2018-08-27 15:25:44 -0400

    Add print statement to show user the reimport tests file name

--- pandokia/import_data.py
+++ pandokia/import_data.py
@@ -506,6 +506,7 @@ def run(argv, hack_callback=None):
         if reimport_count != 0:
             print("There are {:d} reimport tests during this run".format(reimport_count))
             reimport_file = "reimport_tests_"+handle+".txt"
+            print("The reimport tests file is called : %s" % reimport_file)
             with open(reimport_file, 'w') as f:
                 for key, value in reimport_parm.items():
                     f.write(str(key)+" : "+str(value)+"\n")
