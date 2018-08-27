df197e4d0ff3dcecac35f9984acbbe47c208a83f
Oi In Tam <oiin.tam@gmail.com>
2018-08-27 13:53:10 -0400

    Update import_data.py

--- pandokia/import_data.py
+++ pandokia/import_data.py
@@ -513,7 +513,7 @@ def run(argv, hack_callback=None):
 
     if reimport_count != 0:
         print("There are {:d} reimport tests during this run".format(reimport_count))
-        reimport_file = "reimport_tests_"+handle+".txt"
+        reimport_file = "reimport_tests_"+args.filename+".txt"
         with open(reimport_file, 'w') as f:
             for key, value in reimport_parm.items():
                 f.write(str(key)+" : "+str(value)+"\n")        
