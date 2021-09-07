b7adacbc988759221854fde118aa258368ffbb24
dchitti-stsci <83288241+dchitti-stsci@users.noreply.github.com>
2021-09-07 13:06:10 -0400

    Revert "remove unused - 'etc_jwst_daily' and 'jwst'. make the recurring prefix mission specific"
    This reverts commit a24293fe8769ee6b71eb03a03cbb8b82ddbbe5d6.

--- pandokia/default_config.py
+++ pandokia/default_config.py
@@ -260,9 +260,10 @@ recurring_prefix = (
     'etc_daily',
     'etc_midday',
     'etc_hst_daily',
+    'etc_jwst_daily',
     'pandeia_nightly',
-    'pandeia_hst_master_latest',
-    'pandeia_jwst_master_latest'
+    'jwst',
+    'pandeia_master_latest'
 )
 
 #####
