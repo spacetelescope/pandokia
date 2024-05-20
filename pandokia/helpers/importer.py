9a8206d1ced8ecb2a80f5c99f1ebb96b0450e4b5
Adric Riedel <adric.riedel@gmail.com>
2024-05-20 13:41:47 -0400

    importlib opens the file for us

--- pandokia/helpers/importer.py
+++ pandokia/helpers/importer.py
@@ -20,12 +20,10 @@ def importer(modulename, filename):
 
     if modulename in sys.modules:
         return sys.modules[modulename]
-    f = open(filename, "r")
     save = sys.dont_write_bytecode
     try:
         sys.dont_write_bytecode = True
-        m = load_source(modulename, filename, f)
+        m = load_source(modulename, filename)
     finally:
-        f.close()
         sys.dont_write_bytecode = save
     return m
