fadc33e5b33896ff761f45bf06364d07b0adbdfb
dchitti-stsci <83288241+dchitti-stsci@users.noreply.github.com>
2026-02-06 14:52:49 -0500

    Update to next planned release version 2026.7

--- etc_utils/setup.py
+++ etc_utils/setup.py
@@ -10,7 +10,7 @@ with open('README.md') as fp:
 setup(
     # The package
     name="etc_utils",
-    version="2026.2.dev0",
+    version="2026.7",
     packages=["etc_utils",
               "etc_utils.helpers",
               ],
